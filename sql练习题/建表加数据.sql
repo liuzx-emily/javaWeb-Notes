b8719a129111e4d087f5baba2a3dbc6d3327213b
liuzx-emily <liuzixuan_emily@outlook.com>
2018-01-04 20:32:26 +0800

    添加sql练习题

---
+++ sql练习题/建表加数据.sql
@@ -0,0 +1,148 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : liuzx
+Source Server Version : 50548
+Source Host           : localhost:3306
+Source Database       : liuzx
+
+Target Server Type    : MYSQL
+Target Server Version : 50548
+File Encoding         : 65001
+
+Date: 2018-01-04 17:25:14
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `deptno` int(11) NOT NULL,
+  `dname` varchar(50) DEFAULT NULL,
+  `loc` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`deptno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES ('10', '教研部', '北京');
+INSERT INTO `dept` VALUES ('20', '学工部', '上海');
+INSERT INTO `dept` VALUES ('30', '销售部', '广州');
+INSERT INTO `dept` VALUES ('40', '财务部', '武汉');
+
+-- ----------------------------
+-- Table structure for emp
+-- ----------------------------
+DROP TABLE IF EXISTS `emp`;
+CREATE TABLE `emp` (
+  `empno` int(11) NOT NULL,
+  `ename` varchar(50) DEFAULT NULL,
+  `job` varchar(50) DEFAULT NULL,
+  `mgr` int(11) DEFAULT NULL,
+  `hiredate` date DEFAULT NULL,
+  `sal` decimal(7,2) DEFAULT NULL,
+  `COMM` decimal(7,2) DEFAULT NULL,
+  `deptno` int(11) DEFAULT NULL,
+  PRIMARY KEY (`empno`),
+  KEY `fk_emp` (`mgr`),
+  CONSTRAINT `fk_emp` FOREIGN KEY (`mgr`) REFERENCES `emp` (`empno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of emp
+-- ----------------------------
+INSERT INTO `emp` VALUES ('1001', '甘宁', '文员', '1013', '2000-12-17', '8000.00', null, '20');
+INSERT INTO `emp` VALUES ('1002', '黛绮丝', '销售员', '1006', '2001-02-20', '16000.00', '3000.00', '30');
+INSERT INTO `emp` VALUES ('1003', '殷天正', '销售员', '1006', '2001-02-22', '12500.00', '5000.00', '30');
+INSERT INTO `emp` VALUES ('1004', '刘备', '经理', '1009', '2001-04-02', '29750.00', null, '20');
+INSERT INTO `emp` VALUES ('1005', '谢逊', '销售员', '1006', '2001-09-28', '12500.00', '14000.00', '30');
+INSERT INTO `emp` VALUES ('1006', '关羽', '经理', '1009', '2001-05-01', '28500.00', null, '30');
+INSERT INTO `emp` VALUES ('1007', '张飞', '经理', '1009', '2001-09-01', '24500.00', null, '10');
+INSERT INTO `emp` VALUES ('1008', '诸葛亮', '分析师', '1004', '2007-04-19', '30000.00', null, '20');
+INSERT INTO `emp` VALUES ('1009', '曾阿牛', '董事长', null, '2001-11-17', '50000.00', null, '10');
+INSERT INTO `emp` VALUES ('1010', '韦一笑', '销售员', '1006', '2001-09-08', '15000.00', '0.00', '30');
+INSERT INTO `emp` VALUES ('1011', '周泰', '文员', '1008', '2007-05-23', '11000.00', null, '20');
+INSERT INTO `emp` VALUES ('1012', '程普', '文员', '1006', '2001-12-03', '9500.00', null, '30');
+INSERT INTO `emp` VALUES ('1013', '庞统', '分析师', '1004', '2001-12-03', '30000.00', null, '20');
+INSERT INTO `emp` VALUES ('1014', '黄盖', '文员', '1007', '2002-01-23', '13000.00', null, '10');
+
+-- ----------------------------
+-- Table structure for salgrade
+-- ----------------------------
+DROP TABLE IF EXISTS `salgrade`;
+CREATE TABLE `salgrade` (
+  `grade` int(11) NOT NULL,
+  `losal` int(11) DEFAULT NULL,
+  `hisal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`grade`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of salgrade
+-- ----------------------------
+INSERT INTO `salgrade` VALUES ('1', '7000', '12000');
+INSERT INTO `salgrade` VALUES ('2', '12010', '14000');
+INSERT INTO `salgrade` VALUES ('3', '14010', '20000');
+INSERT INTO `salgrade` VALUES ('4', '20010', '30000');
+INSERT INTO `salgrade` VALUES ('5', '30010', '99990');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `student_id` varchar(50) DEFAULT NULL COMMENT '全名',
+  `full_name` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `id` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES ('1', '小美2', '23', '33D67A1C82624E9691BF9A494B0CCCFD');
+INSERT INTO `student` VALUES ('31', '钱', '33', '7B923D51786240D3883977657831D67C');
+INSERT INTO `student` VALUES ('15', '孙', '18', '92C13AE9AC22454BB7B1CACBFC4460A2');
+INSERT INTO `student` VALUES ('16', '李', '32', '99D246594BC5452DB3B7B069B0628E93');
+INSERT INTO `student` VALUES ('3', '赵', '22', '9C2A8BDC5C194604951F458826E213DA');
+INSERT INTO `student` VALUES ('21', '吴', '21', 'D65763B6CB1A4B869E86ACED49512D84');
+INSERT INTO `student` VALUES ('21', '周', '22', 'DB702777B7CA4A44BA560D4DC1E69B4D');
+INSERT INTO `student` VALUES ('2', '小美', '23', 'E8B95437283B4C998D7533B88D2D4B89');
+
+-- ----------------------------
+-- Table structure for tb
+-- ----------------------------
+DROP TABLE IF EXISTS `tb`;
+CREATE TABLE `tb` (
+  `year` varchar(255) DEFAULT NULL,
+  `zz` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb
+-- ----------------------------
+INSERT INTO `tb` VALUES ('2010', '100');
+INSERT INTO `tb` VALUES ('2011', '150');
+INSERT INTO `tb` VALUES ('2012', '250');
+INSERT INTO `tb` VALUES ('2013', '800');
+INSERT INTO `tb` VALUES ('2014', '1000');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('liuzx', '123');
---
+++ sql练习题/练习题.sql
@@ -0,0 +1,227 @@
+1.查询出部门编号为30的所有员工 
+-- SELECT * FROM emp WHERE deptno=30;
+
+2.所有销售员的姓名、编号和部门编号。 
+-- SELECT e.ename,e.empno,e.deptno FROM emp e WHERE e.job=’销售员’;
+
+3.找出奖金高于工资的员工。 
+-- SELECT * FROM emp WHERE COMM>sal;
+
+4.找出奖金高于工资60%的员工。 
+-- SELECT * from emp where COMM>sal*0.6;
+
+5.找出部门编号为10中所有经理，和部门编号为20中所有销售员的详细资料。 
+-- SELECT * FROM emp where (deptno=10 and job=’经理’) or (deptno=20 and job=’销售员’);
+
+6.找出部门编号为10中所有经理，部门编号为20中所有销售员， 
+还有即不是经理又不是销售员但其工资大或等于20000的所有员工详细资料。 
+-- SELECT * FROM emp where (deptno=10 and job=’经理’) or (deptno=20 and job=’销售员’) 
+-- or (job NOT IN (‘销售员’,’经理’) AND sal>=20000);
+
+7.有奖金的工种。 
+-- SELECT DISTINCT job FROM emp where COMM is not NULL ;
+
+8.无奖金或奖金低于1000的员工。 
+-- SELECT * from emp WHERE COMM is NULL or emp.COMM<1000;
+
+9.查询名字由三个字组成的员工。 
+-- select * from emp where ename LIKE ‘_‘;
+
+10.查询2000年入职的员工。 
+-- select * from emp where hiredate LIKE ‘2000%’;
+
+11.查询所有员工详细信息，用编号升序排序 
+-- SELECT * FROM emp ORDER BY empno ASC ;
+
+12.查询所有员工详细信息，用工资降序排序，如果工资相同使用入职日期升序排序 
+-- select * from emp ORDER BY sal desc,hiredate asc;
+
+13.查询每个部门的平均工资 
+-- select deptno,avg(sal) from emp GROUP BY deptno;
+
+14.求出每个部门的雇员数量。 
+-- select deptno,count(1) from emp GROUP BY deptno;
+
+15.查询每种工作的最高工资、最低工资、人数 
+-- SELECT job,max(sal),min(sal),count(1) from emp group by job;
+
+16.显示非销售人员工作名称以及从事同一工作雇员的月工资的总和，并且要满足从事同一工作的雇员的月工资合计大于50000，输出结果按月工资的合计升序排列 
+-- select job,sum(sal) from emp where job<>’销售员’ GROUP BY job HAVING sum(sal)>50000 ORDER BY sum(sal) asc;
+
+
+
+/*
+	1. 查出至少有一个员工的部门。显示部门编号、部门名称、部门位置、部门人数。
+*/
+
+SELECT
+	d.*, z1.cnt
+FROM
+	dept d,
+	(
+		SELECT
+			deptno,
+			COUNT(*) cnt
+		FROM
+			emp
+		GROUP BY
+			deptno
+	) z1
+WHERE
+	d.deptno = z1.deptno 
+
+/*
+	2. 列出所有员工的姓名及其直接上级的姓名。
+*/
+	SELECT
+		*
+	FROM
+		emp e,
+		emp m
+	WHERE
+		e.mgr = m.empno SELECT
+			e.ename,
+			IFNULL(m.ename, 'BOSS') 领导
+		FROM
+			emp e
+		LEFT OUTER JOIN emp m ON e.mgr = m.empno
+
+/*
+	3. 列出受雇日期早于直接上级的所有员工的编号、姓名、部门名称。
+*/
+	SELECT
+		e.empno,
+		e.ename,
+		d.dname
+	FROM
+		emp e,
+		emp m,
+		dept d
+	WHERE
+		e.mgr = m.empno
+	AND e.hiredate < m.hiredate
+	AND e.deptno = d.deptno 
+/*
+	4. 列出部门名称和这些部门的员工信息，同时列出那些没有员工的部门。
+*/
+	SELECT
+		*
+	FROM
+		emp e
+	RIGHT OUTER JOIN dept d ON e.deptno = d.deptno
+
+/*
+	5. 列出最低薪金大于15000的各种工作及从事此工作的员工人数。
+*/
+	SELECT
+		job,
+		COUNT(*)
+	FROM
+		emp e
+	GROUP BY
+		job
+	HAVING
+		MIN(sal) > 15000
+/*
+	6. 列出在销售部工作的员工的姓名，假定不知道销售部的部门编号。
+*/
+	SELECT
+		*
+	FROM
+		emp e
+	WHERE
+		e.deptno = (
+			SELECT
+				deptno
+			FROM
+				dept
+			WHERE
+				dname = '销售部'
+		)
+/*
+	7. 列出薪金高于公司平均薪金的所有员工信息，所在部门名称，上级领导，工资等级。
+*/
+	SELECT
+		e.*, d.dname,
+		m.ename,
+		s.grade
+	FROM
+		emp e,
+		dept d,
+		emp m,
+		salgrade s
+	WHERE
+		e.sal > (SELECT AVG(sal) FROM emp)
+	AND e.deptno = d.deptno
+	AND e.mgr = m.empno
+	AND e.sal BETWEEN s.losal
+	AND s.hisal --------------- SELECT
+		e.*, d.dname,
+		m.ename,
+		s.grade
+	FROM
+		emp e
+	LEFT OUTER JOIN dept d ON e.deptno = d.deptno
+	LEFT OUTER JOIN emp m ON e.mgr = m.empno
+	LEFT OUTER JOIN salgrade s ON e.sal BETWEEN s.losal
+	AND s.hisal
+	WHERE
+		e.sal > (SELECT AVG(sal) FROM emp) SELECT
+			*
+		FROM
+			emp;
+
+
+/*
+	10.列出与庞统从事相同工作的所有员工及部门名称。
+*/
+	SELECT
+		e.*, d.dname
+	FROM
+		emp e,
+		dept d
+	WHERE
+		e.deptno = d.deptno
+	AND job = (
+		SELECT
+			job
+		FROM
+			emp
+		WHERE
+			ename = '庞统'
+	) 
+/*
+	11.列出薪金高于在部门30工作的所有员工的薪金　的员工姓名和薪金、部门名称。
+*/
+	SELECT
+		e.ename,
+		e.sal,
+		d.dname
+	FROM
+		emp e,
+		dept d
+	WHERE
+		e.deptno = d.deptno
+	AND sal > ALL (
+		SELECT
+			sal
+		FROM
+			emp
+		WHERE
+			deptno = 30
+	)
+/*
+	13.查出年份、利润、年度增长比
+*/
+	SELECT
+		y1.*, IFNULL(
+			CONCAT(
+				(y1.zz - y2.zz) / y2.zz * 100,
+				'%'
+			),
+			'0%'
+		) 增长比
+	FROM
+		tb_year y1
+	LEFT OUTER JOIN tb_year y2 ON y1. YEAR = y2. YEAR + 1;
+
